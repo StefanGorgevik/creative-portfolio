3db20ba07ba8608774f043f012ab5c414a025ade
Stefan Gorgevik <stefangorgevik@hotmail.com>
2020-04-11 20:24:10 +0200

    app created

---
+++ creative-portfolio/src/containers/AboutMe/AboutMe.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import './AboutMe.css'
+import AboutMeHeader from '../../components/AboutMeHeader/AboutMeHeader'
+import EducationCourses from '../../components/EducationCourses/EducationCourses'
+import PersonalInfo from '../../components/PersonalInfo/PersonalInfo'
+import TechnicalSkills from '../../components/TechnicalSkills/TechnicalSkills'
+import Projects from '../../components/Projects/Projects'
+
+class AboutMe extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            divs: ["Personal Info", "Education & Courses", "Technical Skills", "Projects"],
+            hoveredDiv: ''
+        }
+    }
+
+    divHovered = (div) => {
+        this.setState({
+            hoveredDiv: div
+        })
+    }
+
+    renderSwitch() {
+        switch (this.state.hoveredDiv) {
+            case 'Personal Info':
+                return <PersonalInfo/>;
+            case 'Education & Courses':
+                return <EducationCourses/>;
+            case 'Technical Skills':
+                return <TechnicalSkills/>;
+            case 'Projects':
+                return <Projects/>;
+            default:
+                return null;
+        }
+    }
+
+    render() {
+        return (
+            <div className="about-me-main-div">
+                <AboutMeHeader
+                    divs={this.state.divs}
+                    hoveredDiv={this.state.hoveredDiv}
+                    divHovered={this.divHovered} />
+                <div className="about-me-div">
+                    {this.renderSwitch()}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default AboutMe;
