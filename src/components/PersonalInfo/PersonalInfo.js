31cae2c2b7b6777f1cdfdea85af9dc7f2b804c34
Stefan Gorgevik <stefangorgevik@hotmail.com>
2020-04-12 23:36:07 +0200

    css improvements, added github link

--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -4,20 +4,25 @@ import '../shared.css'
 
 
 function PersonalInfo(props) {
-    const keys = ["Name", "Professional Headline", "Email", "Phone Number", "Location", "Age",]
-    const values = ["Stefan Gorgevik", "Full stack web developer", "stefangorgevik@hotmail.com", "079-231-692", "Skopje, Macedonia", 25]
+    const keys = ["Name", "Professional Headline", "Email", "GitHub", "Phone Number", "Location", "Age",]
+    const values = ["Stefan Gorgevik", "Full stack web developer", "stefangorgevik@hotmail.com", "https://github.com/StefanGorgevik", "079-231-692", "Skopje, Macedonia", 25]
     const infoDivs = keys.map((key, i) => {
         return (
             <div key={key + i} className="info-div">
                 <p className="key">{key}</p>
-                <p className="value">{values[i]}</p>
+                <p className="value">{values[i] === 'https://github.com/StefanGorgevik' ?
+                    <a href="https://github.com/StefanGorgevik"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        {values[i]}</a>
+                    : values[i]}</p>
             </div>
         )
     })
     return (
         <div className="personal-main-info-div">
             <h1 className="about-me-divs-title">Personal Info</h1>
-                {infoDivs}
+            {infoDivs}
         </div>
     )
 }
