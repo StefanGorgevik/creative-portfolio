483354a1e0979cb0ce54f4970b4d6d9ea1ade7b2
Stefan Gorgevik <stefangorgevik@hotmail.com>
2020-04-30 12:28:39 +0200

    improved css for projects and added a project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,23 +10,23 @@ function Projects(props) {
                 <Project name="Budget Architect"
                     info="Full-stack developed project, made with NodeJS and ReactJS. 
                     I used MongoDB as a database and mongoose for creating the model.  
-                    This is a project where you can save your products and also add a group of products.
-                     Every product and group can be edited or deleted, and all items can be sorted and filtered.
-                     For style, I used plain CSS, and for deployment, I used Heroku."
+                    For style, I used plain CSS, and for deployment, I used Heroku."
                     url="https://budgetarchitect.herokuapp.com/" />
-                <Project name="BodyBuildingMedia - V1"
+                <Project name="BodyBuildingMedia"
                     info="The first version of a bodybuilding social media, which is a full-stack developed project, 
                     created with ReactJS, Node.js, and MongoDB. It is a responsive website and for UI, I used plain HTML and CSS. 
-                    It is a type of social media where you can keep track of your diet and workout plan, and you can see other users' diets, 
-                    workout plans,and personal info. Photo upload mode was created with the help of Cloudinary and 
-                     the project is deployed on Heroku."
+                    Photo upload mode was created with the help of Cloudinary and the project is deployed on Heroku."
                     url="https://bodybuildingmedia.herokuapp.com" />
                 <Project name="Expense Calculator"
                     info="Full-stack developed project, created with ReactJS, Node.js, and MongoDB. For the UI, I used plain HTML and
                             CSS. It is a calculator where you can keep track of your expenses and bought items. It was given as an ending project
-                            by Semos Education. I also included a user info page and the option to add another user and merge both of your products."
+                            by Semos Education."
                     url="https://stark-island-29614.herokuapp.com" />
 
+                <Project name="Rick n' Morty"
+                    info="Front-end developed project, created with ReactJS, using React Hooks. For the UI, I used plain HTML and
+                            CSS. It is a small front-end project where I used an API and made a responsive web page."
+                    url="https://stefangorgevik.github.io/ricknmorty/" />
                 <Project name="Movie Database"
                     info="Front-end developed project, created with ReactJS. For the UI, I used plain HTML and
                             CSS. It is a small front-end project where I used a prepared API, but the information was limited, 
